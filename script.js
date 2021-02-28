02aef9b26c577003212dd8e4f5c794a5fdb2616d
Antonio Alves <antoniodecastroalves@gmail.com>
2021-02-28 09:37:11 -0300

    moviment snake

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d") ;
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //....
 let box = 32;
-let snake = [];
-snake[0] = {
-  x: 8 * box,
-  y: 8 * box
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
 }
+let direction = "right";
 
 function createBG() {
   context.fillStyle = "lightgreen";
-  context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
-function createSnake() {
-  for(1=0; i> snake.length; i++){
+function createSnake(){
+  for(i = 0; i < snake.length; i++){
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
-createBG();
+function StartGame(){
+  createBG();
+  createSnake();
 
-createSnake();
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);  
+
+
+}
+
+let start = setInterval(StartGame, 100);
+
